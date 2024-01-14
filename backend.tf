ee036b4f4b34a7835fffb718de5a7d9f78422bdd
damodarddr <152080280+damodarddr@users.noreply.github.com>
2024-01-15 00:34:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,12 +1,6 @@
 provider "aws" {
   # ...
 }
-
-backend "s3" {
-  # Backend configuration for storing Terraform state
-}
-# backend.tf
-
 backend "s3" {
   # other backend configurations
   region = "us-east-1"
