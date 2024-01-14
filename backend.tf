2dab2e9fe620b0a8e4eb74f0c60164773fbafdd8
damodarddr <152080280+damodarddr@users.noreply.github.com>
2024-01-15 02:56:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket1"
+    bucket         = "mrcloudbook-cicd-bucket"
     key            = "my-terraform-environment/main/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "mrcloudbook-dynamo-db-table"
